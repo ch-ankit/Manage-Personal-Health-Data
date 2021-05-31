72f31ba42cda4973bd053ceac8aecee851fba328
Ankit Khatiwada <akgunner6@gmail.com>
2021-05-31 19:55:12 +0545

    Gecko driver added for windows

--- client/src/DocumentViewer.js
+++ client/src/DocumentViewer.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { useHistory } from 'react-router'
 import "./DocumentViewer.scss"
 function DocumentViewer() {
-    const history = useHistory()
     return (
         <div className="documentViewer">
-            <embed src="http://localhost:7000/report?id=2000-03-16456132&reportName=1622447862606-Interrupt%20Cycle.pdf"
-                className="documentViewer__document"
+            <object data="http://localhost:7000/report?id=2000-03-16456132&reportName=1622447862606-Interrupt%20Cycle.pdf"
+                type="application/pdf" className="documentViewer__document"
             />
         </div>
     )

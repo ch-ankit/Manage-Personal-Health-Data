f578c64270916d68d70bce448e78de7f91511f63
root <khatribabin39@gmail.com>
2021-06-01 11:48:14 +0545

    Document viewer fixed to work on mobile browser also

--- client/src/DocumentViewer.js
+++ client/src/DocumentViewer.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router'
+import {Document , Page} from 'react-pdf/dist/esm/entry.webpack';
 import "./DocumentViewer.scss"
-function DocumentViewer() {
-    const history = useHistory()
-    return (
-        <div className="documentViewer">
-            <embed src="http://localhost:7000/report?id=2000-03-16456132&reportName=1622447862606-Interrupt%20Cycle.pdf"
-                className="documentViewer__document"
-            />
-        </div>
-    )
-}
 
-export default DocumentViewer
+
+function DocumentViewer() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div className="documentViewer" >
+      <Document
+        file="http://localhost:7000/report?id=2000-03-16456132&reportName=1622447862606-Interrupt%20Cycle.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+        className="documentViewer__document"
+      
+      >
+
+    {Array.apply(null, Array(numPages))
+    .map((x, i)=>i+1)
+    .map(page => <Page pageNumber={page} className="documentViewer__page" />)}
+      </Document>
+    </div>
+  );
+}
+export default DocumentViewer;

74bc74b691ab9dbeb1d52b0483854a53506d118b
Ankit Khatiwada <akgunner6@gmail.com>
2021-06-20 22:28:39 +0545

    Doc SignUp to db done

--- client/src/SignUpDoc.js
+++ client/src/SignUpDoc.js
@@ -2,32 +2,42 @@ import React, { useRef, useState } from 'react'
 import storage from './firebaseConfig'
 import Nav from './Nav'
 import "./SignUpDoc.scss"
+import { languages } from './LanguageDataset'
+
 
 function SignUp() {
     //Practitioner Parameters
-    const docFirstName = useRef(null) //Firstname
-    const docLastName = useRef(null) //Lastname
-    const docMiddleName = useRef(null) //add this to Firstname with a space
-    const docMobileNo = useRef(null)
-    const docEmail = useRef(null)
-    const docHouseNumber = useRef(null)
-    const docState = useRef(null)
-    const docDistrict = useRef(null)
-    const docCity = useRef(null)
-    const docPostalCode = useRef(null)
-    const docGender = useRef(null)
-    const nmcRegdNo = useRef(null)
+    const firstName = useRef(null) //Firstname
+    const lastName = useRef(null) //Lastname
+    const middleName = useRef(null) //add this to Firstname with a space
+    const prefix = useRef(null) //add this to Firstname with a space
+    const suffix = useRef(null) //add this to Firstname with a space
+    const dob = useRef(null)
+    const mobileNo = useRef(null)
+    const email = useRef(null)
+    const streetName = useRef(null)
+    const houseNo = useRef(null)
+    const state = useRef(null)
+    const district = useRef(null)
+    const city = useRef(null)
+    const country = useRef(null)
+    const language = useRef(null)
+    const postalCode = useRef(null)
+    const gender = useRef(null)
+    const id = useRef(null)
+    const periodStart = useRef(null)
     //Firebase resource
-    const [docPhoto, setDocPhoto] = useState('')
+    const [photo, setPhoto] = useState('')
     const [viewFile, setViewFile] = useState('')
-
+    const [customPrefix, setCustomPrefix] = useState(false)
+    const [customSuffix, setCustomSuffix] = useState(false)
 
     //Functions
     const handleChange = (e) => {
         e.preventDefault();
         if (e.target.files[0]) {
             if (e.target.files[0].type.match(/image-*/)) {
-                setDocPhoto(e.target.files[0]);
+                setPhoto(e.target.files[0]);
                 setViewFile(URL.createObjectURL(e.target.files[0]))
             }
             else {
@@ -38,7 +48,7 @@ function SignUp() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const uploadTask = storage.ref(`images/${docPhoto.name}`).put(docPhoto);
+        const uploadTask = storage.ref(`images/${photo.name}`).put(photo);
         uploadTask.on(
             "state_changed",
             (snapshot) => {
@@ -50,16 +60,50 @@ function SignUp() {
             () => {
                 storage
                     .ref("images")
-                    .child(docPhoto.name)
+                    .child(photo.name)
                     .getDownloadURL()
                     .then(async (url) => {
+                        const system = language.current.value.split('-')
+                        const qualificationCodeSystem = 'https://nmc.org.np/searchPractitioner'
+                        const qualificationCodeCode = "R"
+                        const qualificationCodeDisplay = 'Qualified doctor'
+                        const qualificationCodetext = `Dr.${firstName.current.value} is a registered doctor in NMC`
+                        const qualificationIdentifierSystem = 'https://nmc.org.np/searchPractitioner'
+                        const qualificationIdentifierValue = id.current.value
+                        const issuer = "Nepal Medical Council"
                         const response = await fetch('http://localhost:7000/signup/doctor', {
                             method: 'POST',
                             headers: {
                                 'Content-type': 'application/json'
                             },
                             body: JSON.stringify({
-
+                                firstName: firstName.current.value,
+                                lastName: lastName.current.value,
+                                middleName: middleName.current.value,
+                                prefix: prefix.current.value,
+                                suffix: suffix.current.value,
+                                dob: dob.current.value,
+                                mobileNo: mobileNo.current.value,
+                                email: email.current.value,
+                                streetName: streetName.current.value,
+                                houseNo: houseNo.current.value,
+                                state: state.current.value,
+                                district: district.current.value,
+                                city: city.current.value,
+                                country: country.current.value,
+                                language: system[0],
+                                languageCode: system[1],
+                                postalCode: postalCode.current.value,
+                                gender: gender.current.value,
+                                id: id.current.value,
+                                periodStart: periodStart.current.value,
+                                qualificationCodeSystem: qualificationCodeSystem,
+                                qualificationCodeCode: qualificationCodeCode,
+                                qualificationCodeDisplay: qualificationCodeDisplay,
+                                qualificationCodetext: qualificationCodetext,
+                                qualificationIdentifierSystem: qualificationIdentifierSystem,
+                                qualificationIdentifierValue: qualificationIdentifierValue,
+                                issuer: issuer
                             })
                         })
 
@@ -70,36 +114,77 @@ function SignUp() {
         )
 
     }
+    const languageMap = Object.keys(languages).map(el => <option key={el} value={`${languages[el].name}-${languages[el].code}`} > {languages[el].name}</ option>)
     return (
         <div className="signUpDoc">
             <Nav />
             <form id="SignUpForm" onSubmit={handleSubmit}>
                 <h1>Sign Up</h1>
                 <div className="signUpForm__name">
-                    <input ref={docFirstName} type="text" id="name" placeholder="First Name" required />
-                    <input ref={docMiddleName} type="text" id="name" placeholder="Middle Name" />
-                    <input ref={docLastName} type="text" id="name" placeholder="Last Name" required />
+                    <div className="signUp__select" style={{ display: `${customPrefix ? 'none' : 'flex'}` }}>
+                        <select ref={prefix} defaultChecked="Prefix" id="prefix" >
+                            <option value="Prefix" hidden > Please Select you Prefix</option>
+                            <option value="Mr."> Mr.</option>
+                            <option value="Mrs.">Mrs.</option>
+                            <option value="Ms.">Ms.</option>
+                            <option value="Dr.">Dr.</option>
+                            <option value="Er.">Er.</option>
+                            <option value="Prof.">Prof.</option>
+                            <option value="custom" onClick={() => setCustomPrefix(true)}>Custom</option>
+                        </select>
+                    </div>
+                    {
+                        customPrefix ?
+                            <input ref={prefix} type="text" id="prefix" placeholder="Prefix" required /> : ''
+                    }
+                    <input ref={firstName} type="text" id="name" placeholder="First Name" required />
+                    <input ref={middleName} type="text" id="name" placeholder="Middle Name" />
+                    <input ref={lastName} type="text" id="name" placeholder="Last Name" required />
+                    <div className="signUp__select" style={{ display: `${customSuffix ? 'none' : 'flex'}` }}>
+                        <select ref={suffix} defaultChecked="Suffix" id="suffix" >
+                            <option value="Suffix" hidden > Please Select you Suffix</option>
+                            <option value="Phd."> Phd.</option>
+                            <option value="MD">MD</option>
+                            <option value="MS">MS</option>
+                            <option value="custom" onClick={() => setCustomSuffix(true)}>Custom</option>
+                        </select>
+                    </div>
+                    {
+                        customSuffix ?
+                            <input ref={suffix} type="text" id="suffix" placeholder="Suffix" required /> : ''
+                    }
                 </div>
                 <div className="signUpForm__address">
-                    <input ref={docCity} type="text" id="docCity" placeholder="City" required />
-                    <input ref={docDistrict} type="text" id="docDistrict" placeholder="District" required />
-                    <input ref={docState} type="text" id="docState" placeholder="State" required />
-                    <input ref={docHouseNumber} type="text" id="docHouseNumber" placeholder="House/Appartment Number" />
+                    <input ref={city} type="text" id="city" placeholder="City" required />
+                    <input ref={district} type="text" id="district" placeholder="District" required />
+                    <input ref={state} type="text" id="state" placeholder="State" required />
+                    <input ref={country} type="text" id="country" placeholder="country" required />
+                    <input ref={houseNo} type="text" id="houseNumber" placeholder="House/Appartment Number" />
+                    <input ref={streetName} type="text" id="streetName" placeholder="Street Name" />
                 </div>
 
-                <input ref={docPostalCode} type="text" id="docPostalCode" placeholder="Postal Code" required />
+                <input ref={postalCode} type="text" id="postalCode" placeholder="Postal Code" required />
 
-                <input ref={docEmail} type="email" id="email" placeholder="Email" required />
+                <input ref={email} type="email" id="email" placeholder="Email" required />
+                <input ref={dob} type="date" onChange={() => { console.log(dob.current.value) }} id="dob" placeholder="Date of Birth" required />
+                <input ref={mobileNo} type="text" id="mobileNo" placeholder="Contact Number" required />
 
-                <input ref={docMobileNo} type="text" id="docMobileNo" placeholder="Contact Number" required />
-
-                <input ref={nmcRegdNo} type="text" id="nmcRegdNo" placeholder="NMC Registration Number" required />
+                <input ref={id} type="text" id="id" placeholder="NMC Registration Number" required />
+                <input ref={periodStart} type="date" id="id" placeholder="NMC Registered Date" required />
+                <div className="signUp__select">
+                    <div className="signUp__customSelect">
+                        <select ref={language} defaultChecked="Language" id="language" required>
+                            <option value="Language" hidden > Please Select preferred Language</option>
+                            {languageMap}
+                        </select>
+                    </div>
+                </div>
                 <input type="file" accept="image/*" id="image" alt="Profile photo" onChange={handleChange} required />
                 <img src={viewFile} style={{ height: '81px', width: '256px' }} alt="Uploaded" />
                 <div className="signUp__select">
                     <h4>Gender</h4>
                     <div className="signUp__customSelect">
-                        <select ref={docGender} defaultChecked="Select Gender" id="gender" required>
+                        <select ref={gender} defaultChecked="Select Gender" id="gender" required>
                             <option value="Select Gender" hidden > Please Select Your Gender</option>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>

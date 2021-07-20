b296128539bca8714f45d7dd82717f18d77e6a71
Ankit Khatiwada <akgunner6@gmail.com>
2021-07-20 13:11:00 +0545

    added report upload

--- client/src/Report.js
+++ client/src/Report.js
@@ -15,6 +15,15 @@ function Report() {
     });
     console.log(response);
   };
+  const handleMedicalUpload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const response = await fetch("http://localhost:7000/report", {
+      method: "POST",
+      body: formData,
+    });
+    console.log(response);
+  };
   console.log(userData.uId);
   return (
     <div className="report">
@@ -22,6 +31,15 @@ function Report() {
         <h1>Upload your report</h1>
         <form ref={form} onSubmit={handleUpload} encType="multipart/form-data">
           <input value={userData?.uId} name="id" style={{ display: "none" }} />
+          <input type="file" name="file" multiple />
+          <button type="submit">Upload</button>
+        </form>
+      </div>
+      <div className="newreport__upload">
+        <h1>Upload your Medical report</h1>
+        <form ref={form} onSubmit={handleMedicalUpload} encType="multipart/form-data">
+          <input value={20000101 - 794155} name="id" style={{ display: "none" }} />
+          <input value={1626764008937} name="masterId" style={{ display: "none" }} />
           <input type="file" name="file" multiple />
           <button type="submit">Upload</button>
         </form>

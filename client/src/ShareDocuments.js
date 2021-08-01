c827c692d039c639da8374c11d85342a14e6b8d3
root <khatribabin39@gmail.com>
2021-08-01 16:30:50 +0545

    Sharing data to doctor implemented

--- client/src/ShareDocuments.js
+++ client/src/ShareDocuments.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import "./ShareDocuments.scss"
 import {TrashIcon} from "@heroicons/react/outline"
 import { deleteShareDocs } from './features/counterSlice';
 function ShareDocuments() {
     const documentsList=useSelector(state=>state.user.shareDocuments);
+    const userData=useSelector(state=>state.user.value)
     const dispatch=useDispatch();
     const [dummy, setdummy] = useState(false);
     const [doctorData,setdoctorData]=useState([]);
+    const [temporaryData, settemporaryData] = useState([])
+    const timeNumber = useRef(null);
+    const timeUnit=useRef(null)
     useEffect(() => {
         async function getDoctor(){
             const response=await fetch("http://localhost:7000/search/doctor",{
@@ -19,6 +23,43 @@ function ShareDocuments() {
         }
         return getDoctor();
     }, [])
+
+    const shareData=(docData,e)=>{
+        e.preventDefault();
+        let accessTime;
+        switch(timeUnit.current.value){
+            case "hr":
+                accessTime=(timeNumber.current.value)*60;
+                break;
+            case "day":
+                accessTime=(timeNumber.current.value)*1440;
+                break;            
+            case "min":
+                accessTime=timeNumber.current.value;
+                break;
+        }
+        documentsList.forEach(async(documents)=>{
+            console.log(document)
+            const response=await fetch("http://localhost:7000/share",{
+            method:"POST",
+            headers:{
+                "Content-type":"application/json"
+            },
+            body:JSON.stringify(
+                {
+                    "id":userData.uId,
+                    "doctorId":docData.doctorId,
+                    "masterId":documents.filename.replace(".pdf",''),
+                    accessTime
+                }
+            )
+        });
+        const data=await response.json()
+        console.log(data)
+        
+        })
+        
+    }
     return (
         <div className="shareDocuments">
             <h1>List of Documents to be shared</h1>
@@ -36,17 +77,66 @@ function ShareDocuments() {
                 </div>)
             })}
             <div className="shareDocuments__shareButton">
-                <button>Share</button>
+                <button
+                onClick={()=>{
+                    document.querySelector(".shareDocuments__popup").classList.toggle("active")
+                }}
+                >Share</button>
             </div>
             <div className="shareDocuments__popup">
-                <input type="time" />
-                <input  type="search" placeholder="Input the name of the doctor" />
+                <div className="shareDocuments__close"
+                 onClick={()=>{
+                     document.querySelector(".shareDocuments__popup").classList.remove("active")
+                 }}
+                >
+                    X
+                </div>
+                <h1>Share your documents</h1>
+                <div className="shareDocuments__selectTime">
+                    <div className="shareDocuments__Time">
+                        <input ref={timeNumber} type="number"/>
+                        <select ref={timeUnit}>
+                            <option value="day">day</option>
+                            <option value="hr">hr</option>
+                            <option value="min">min</option>
+                        </select>
+                    </div>
+                    
+                    OR
+                    <div className="shareDocuments__checkbox">
+                        <input type="checkbox" onChange={()=>{}}/>
+                        Live Share 
+                    </div>
+                </div>
+                <div className="shareDocuments__searchBar">
+                    <input  type="search" 
+                    onChange={(e)=>{
+                        if(e.target.value !=''){
+                            let tempData=[];
+                            Object.keys(doctorData).map((key)=>{
+                                if(doctorData[key].name.includes(e.target.value)){
+                                    tempData=[ ... tempData , doctorData[key]];
+                                }
+                            })
+                            settemporaryData(tempData)
+                        }else{
+                            settemporaryData([]);
+                        }
+                    }}
+                    placeholder="Input the name of the doctor" />
+                </div>
+                {console.log(documentsList)}
                 <div className="shareDocuments__searchedData">
-                    {Object.keys(doctorData).map((key)=>{
+                    {Object.keys(temporaryData).map((key)=>{
                         return (
                             <div className="shareDocuments__doctorInfo">
-                                <img src={doctorData[key].photo} alt="Doctor Photo" srcset="" />
-                                <p>{doctorData[key].name}</p>
+                                <div className="shareDocuments__imgBox">
+                                    <img src={temporaryData[key].photo} alt="Doctor Photo" className="shareDocuments__doctorImage" />
+                                </div>
+                                <p>{temporaryData[key].name}</p>
+                                <button
+                                onClick={(e)=>shareData(temporaryData[key],e)}
+                                >Share</button>
                             </div>
                         )
                     })}

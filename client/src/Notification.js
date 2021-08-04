062677f15f6d54aa1a2d4476d5fd64c128bea87a
Ankit Khatiwada <akgunner6@gmail.com>
2021-08-04 20:30:43 +0545

    Work on notification display

--- client/src/Notification.js
+++ client/src/Notification.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { documentGet } from './features/counterSlice';
 import './Notification.scss'
 
 function Notification(props) {
     const [notification, setNotification] = useState('')
+    const dispatch = useDispatch()
     const docData = useSelector(state => state.user.doctor)
     const userData = useSelector(state => state.user.value);
     useEffect(() => {
@@ -16,16 +18,35 @@ function Notification(props) {
             })
             const data = await response.json()
             setNotification(data)
+            console.log(data)
         }
         return getNotifications()
     }, [])
+    const markRead = async (e) => {
+        const url = props.doctor ? `http://localhost:7000/doctor/getnotification?doctorId=${docData?.uId}` :
+            ``//user url to mark all nots as read
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-type": 'application/json'
+            },
+            body: JSON.stringify({
+                doctorId: docData?.uId
+            })
+        })
+        const { message } = await response.json()
+        alert(message)
+    }
     var displayNotifications
     if (notification) {
-        displayNotifications = Object.keys(notification).map(el => <Link><div className="notification__display">{notification[el].patientName} has shared a file with you</div></Link>)
+        displayNotifications = Object.keys(notification).map(el => <Link style={{ width: "100%" }} onClick={() => dispatch(documentGet(notification[el].documentId.concat(".pdf")))} key={el} to="/Doctor/documentViewer" > <div style={{ border: notification[el].markAsRead && "1px solid gray" }} className="notification__display">{notification[el].patientName} has shared a file with you</div></Link >)
     }
     return (
-        <div className='notification' style={{ backgroundColor: 'white', height: '94.5vh', width: '100%' }}>
-            {displayNotifications}
+        <div className='notification' style={{ backgroundColor: 'white', height: '100%', width: '100%' }}>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "80%", margin: '0 auto', position: 'relative', paddingTop: '2rem' }}>
+                <p style={{ position: 'absolute', right: '0', top: "1rem", border: 'black 1px solid', padding: '2px' }} onClick={markRead}>Mark all as Read</p>
+                {displayNotifications}
+            </div>
         </div>
     );
 }

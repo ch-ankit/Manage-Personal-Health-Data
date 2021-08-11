71652cc01dc522a3c49d8b0dd3cc3912199db031
Nischal <nbhn333@gmail.com>
2021-08-11 12:22:51 +0545

    rush hour push

--- client/src/EmergencyContact.js
+++ client/src/EmergencyContact.js
@@ -1,89 +1,101 @@
-import React, { useRef } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import "./EmergencyContact.scss"
-import { setDummy } from './features/counterSlice';
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./EmergencyContact.scss";
+import { setDummy } from "./features/counterSlice";
 function EmergencyContact() {
-    const gender = useRef(null);
-    const relationship=useRef(null);
-    const lastName=useRef(null);
-    const firstName=useRef(null);
-    const middleName=useRef(null);
-    const phoneNo=useRef(null);
-    const city=useRef(null);
-    const district=useRef(null);
-    const state=useRef(null);
-    const country=useRef(null);
-    const streetName=useRef(null);
-    const postalCode=useRef(null);
-    const workplace=useRef(null);
-    const userData=useSelector(state=>state.user.value);
-    const dispatch=useDispatch();
-    const uploadEmergencyContact=async (e)=>{
-        e.preventDefault();
-        console.log(userData.uId)
-        const response=await fetch("http://localhost:7000/signup/addcontact",{
-            method:"POST",
-            headers:{
-                'Content-type':"application/json"
-            },
-            body:JSON.stringify({
-                "relationship":relationship.current.value,
-                "lastName":lastName.current.value,
-                "firstName":firstName.current.value,
-                "middleName":middleName.current.value,
-                "phoneNo":phoneNo.current.value,
-                "city":city.current.value,
-                "district":district.current.value,
-                "state":state.current.value,
-                "country":country.current.value,
-                "streetName":streetName.current.value,
-                "postalCode":postalCode.current.value,
-                "gender":gender.current.value,
-                "workplace":workplace.current.value,
-                "id":userData.uId
-}
-            )}
-        )
-        console.log(response)
-        const data=await response.json();
-        console.log(data)
-        dispatch(setDummy())
-        alert("Added Emergency Contact")
-    }
-    return (
-        <div className="emergencyContact">
-            <h2>Emergency Contact</h2>
-            <form onSubmit={uploadEmergencyContact}>
-                <input ref={relationship} type="text" placeholder="Relationship" />
-                <div className="emergencyContact__fullName">
-                    <input ref={firstName} type="text" placeholder="First Name" />
-                    <input ref={middleName} type="text" placeholder="Middle Name" />
-                    <input ref={lastName} type="text" placeholder="Last Name" />
-                </div>
-                <input ref={phoneNo} type="text" pattern="[+]{1}[0-9]{13}" placeholder="Phone no" pattern="[0-9]{10}" />
-               <div className="emergencyContact__Address">
-                   <input ref={city} type="text" placeholder="City" />
-                   <input ref={district} type="text" placeholder="District" />
-                   <input ref={state} type="text" placeholder="state" />
-                   <input ref={country} type="text" placeholder="Country" />
-                   <input ref={streetName} type="text" placeholder="Street Name" />
-                    <input ref={postalCode} type="text" placeholder="Postal COde" />
-               </div>
-               <div className="emergencyContact__gender">
-               <select ref={gender} defaultChecked="Select Gender" id="gender" required>
-                            <option value="Select Gender" hidden > Select Gender</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Other">Other</option>
-                        </select>
-               </div>
-               <div className="emergencyContact__workPlace">
-                    <input ref={workplace} type="text" placeholder="Workplace"/>
-               </div>
-               <button type="submit">Submit</button>
-            </form>
+  const gender = useRef(null);
+  const relationship = useRef(null);
+  const lastName = useRef(null);
+  const firstName = useRef(null);
+  const middleName = useRef(null);
+  const phoneNo = useRef(null);
+  const city = useRef(null);
+  const district = useRef(null);
+  const state = useRef(null);
+  const country = useRef(null);
+  const streetName = useRef(null);
+  const postalCode = useRef(null);
+  const workplace = useRef(null);
+  const userData = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  const uploadEmergencyContact = async (e) => {
+    e.preventDefault();
+    console.log(userData.uId);
+    const response = await fetch("http://localhost:7000/signup/addcontact", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        relationship: relationship.current.value,
+        lastName: lastName.current.value,
+        firstName: firstName.current.value,
+        middleName: middleName.current.value,
+        phoneNo: phoneNo.current.value,
+        city: city.current.value,
+        district: district.current.value,
+        state: state.current.value,
+        country: country.current.value,
+        streetName: streetName.current.value,
+        postalCode: postalCode.current.value,
+        gender: gender.current.value,
+        workplace: workplace.current.value,
+        id: userData.uId,
+      }),
+    });
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    dispatch(setDummy());
+    alert("Added Emergency Contact");
+  };
+  return (
+    <div className="emergencyContact">
+      <h2>Emergency Contact</h2>
+      <form onSubmit={uploadEmergencyContact}>
+        <input ref={relationship} type="text" placeholder="Relationship" />
+        <div className="emergencyContact__fullName">
+          <input ref={firstName} type="text" placeholder="First Name" />
+          <input ref={middleName} type="text" placeholder="Middle Name" />
+          <input ref={lastName} type="text" placeholder="Last Name" />
         </div>
-    )
+        <input
+          ref={phoneNo}
+          type="text"
+          pattern="[+]{1}[0-9]{13}"
+          placeholder="Phone no"
+        />
+        <div className="emergencyContact__Address">
+          <input ref={city} type="text" placeholder="City" />
+          <input ref={district} type="text" placeholder="District" />
+          <input ref={state} type="text" placeholder="state" />
+          <input ref={country} type="text" placeholder="Country" />
+          <input ref={streetName} type="text" placeholder="Street Name" />
+          <input ref={postalCode} type="text" placeholder="Postal COde" />
+        </div>
+        <div className="emergencyContact__gender">
+          <select
+            ref={gender}
+            defaultChecked="Select Gender"
+            id="gender"
+            required
+          >
+            <option value="Select Gender" hidden>
+              {" "}
+              Select Gender
+            </option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className="emergencyContact__workPlace">
+          <input ref={workplace} type="text" placeholder="Workplace" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default EmergencyContact
+export default EmergencyContact;
--- client/src/Notification.js
+++ client/src/Notification.js
@@ -1,170 +1,298 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { CheckIcon, XIcon } from "@heroicons/react/solid"
-import bell from './images/bell.jpg'
+import React, { useEffect, useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { CheckIcon, XIcon } from "@heroicons/react/solid";
+import bell from "./images/bell.jpg";
 
-import { documentGet, patientDataDoctorNotification, recentPatients } from './features/counterSlice';
-import './Notification.scss'
+import {
+  documentGet,
+  patientDataDoctorNotification,
+  recentPatients,
+} from "./features/counterSlice";
+import "./Notification.scss";
 
 function Notification(props) {
-    const [notification, setNotification] = useState('')
-    const timeNumber = useRef(null);
-    const timeUnit = useRef(null)
-    let darkMode = useSelector((state) => state.user.darkMode)
-    const dispatch = useDispatch()
-    const docData = useSelector(state => state.user.doctor)
-    const userData = useSelector(state => state.user.value);
-    const [dummy,setdummy]=useState(true);
-    let tempData={}
-    useEffect(() => {
-        async function getNotifications() {
-            const url = props.doctor ? `http://localhost:7000/doctor/getnotification?doctorId=${docData?.uId}` :
-                `http://localhost:7000/personal/requesteddocument?patientId=${userData?.uId}`//Patient url for fetch nots
-            const response = await fetch(url, {
-                method: 'GET'
-            })
-            const data = await response.json()
-            setNotification(data)
-            console.log(data)
-        }
-        return getNotifications()
-    }, [dummy])
+  const [notification, setNotification] = useState("");
+  const timeNumber = useRef(null);
+  const timeUnit = useRef(null);
+  let darkMode = useSelector((state) => state.user.darkMode);
+  const dispatch = useDispatch();
+  const docData = useSelector((state) => state.user.doctor);
+  const userData = useSelector((state) => state.user.value);
+  const [dummy, setdummy] = useState(true);
+  let tempData = {};
+  useEffect(() => {
+    async function getNotifications() {
+      const url = props.doctor
+        ? `http://localhost:7000/doctor/getnotification?doctorId=${docData?.uId}`
+        : `http://localhost:7000/personal/requesteddocument?patientId=${userData?.uId}`; //Patient url for fetch nots
+      const response = await fetch(url, {
+        method: "GET",
+      });
+      const data = await response.json();
+      setNotification(data);
+      console.log(data);
+    }
+    return getNotifications();
+  }, [dummy]);
 
-    const markRead = async (e) => {
-        const url = props.doctor && `http://localhost:7000/doctor/getnotification?doctorId=${docData?.uId}`
-        const response = await fetch(url, {
-            method: 'POST',
+  const markRead = async (e) => {
+    const url =
+      props.doctor &&
+      `http://localhost:7000/doctor/getnotification?doctorId=${docData?.uId}`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        doctorId: docData?.uId,
+      }),
+    });
+    const { message } = await response.json();
+    alert(message);
+    window.location.reload();
+  };
+  const userDecision = async (e, decision, masterId) => {
+    if (timeNumber.current.value > 0) {
+      let accessTime;
+      switch (timeUnit.current.value) {
+        case "hr":
+          accessTime = timeNumber.current.value * 60;
+          break;
+        case "day":
+          accessTime = timeNumber.current.value * 1440;
+          break;
+        case "min":
+          accessTime = timeNumber.current.value;
+          break;
+        default:
+          console.log("Default case reached");
+      }
+      if (timeNumber !== null && timeUnit !== null) {
+        console.log(e, masterId, accessTime);
+        const response = await fetch(
+          `http://localhost:7000/personal/requesteddocument`,
+          {
+            method: "POST",
             headers: {
-                "Content-type": 'application/json'
+              "Content-type": "application/json",
             },
             body: JSON.stringify({
-                doctorId: docData?.uId
-            })
-        })
-        const { message } = await response.json()
-        alert(message)
-        window.location.reload()
-    }
-    const userDecision = async (e, decision, masterId) => {
-        if(accessTime>0){
-        let accessTime;
-        switch (timeUnit.current.value) {
-            case "hr":
-                accessTime = (timeNumber.current.value) * 60;
-                break;
-            case "day":
-                accessTime = (timeNumber.current.value) * 1440;
-                break;
-            case "min":
-                accessTime = timeNumber.current.value;
-                break;
-            default:
-                console.log('Default case reached')
+              patientId: userData?.uId,
+              doctorId: e,
+              status: decision,
+              name: `${userData?.firstName} ${userData?.lastName}`,
+              photo: userData?.photo,
+              masterId: masterId,
+              accessTime: accessTime,
+            }),
+          }
+        );
+        const { message } = await response.json();
+        console.log(message);
+        if (decision === "granted") {
+          document
+            .querySelector(".notification__sharePopup.active")
+            .classList.remove("active");
         }
-        if (timeNumber !== null && timeUnit !== null) {
-            console.log(e, masterId,accessTime)
-            const response = await fetch(`http://localhost:7000/personal/requesteddocument`, {
-                method: 'POST',
-                headers: {
-                    "Content-type": 'application/json'
-                },
-                body: JSON.stringify({
-                    patientId: userData?.uId,
-                    doctorId: e,
-                    status: decision,
-                    name: `${userData?.firstName} ${userData?.lastName}`,
-                    photo: userData?.photo,
-                    masterId: masterId,
-                    accessTime: accessTime
-                })
-            })
-            const { message } = await response.json()
-            console.log(message)
-            if(decision==="granted"){
-                document.querySelector(".notification__sharePopup.active").classList.remove("active")
-            }
-            setdummy(!dummy);
-        }} else {
-            alert('Please select the suitable Access time')
-        }
+        setdummy(!dummy);
+      }
+    } else {
+      alert("Please select the suitable Access time");
     }
-    var displayNotifications
+  };
+  var displayNotifications;
 
-    if (notification && props.doctor) {
-        displayNotifications = Object.keys(notification).map(el => {
-            return <Link style={{ width: "100%" }} onClick={() => {
-                dispatch(patientDataDoctorNotification(notification[el].patientId))
-                dispatch(documentGet({
-                    filename:(notification[el].documentId.concat(".pdf"))
-                }))
-                dispatch(recentPatients(notification[el].patientId));
-            }} key={el} to="/Doctor/documentViewer" >
-                <div className={`${notification[el].markAsRead === "false" ? "notification__display" : "notification__displayRed"}`}>
-                    <div style={{ width: "3em", height: "3em" }}>
-                        <img src={notification[el].photo} alt="Patient DP" className={`notification__patientPhoto  ${darkMode && "notification__imgDark"}`} />
-                    </div>
-                    {notification[el].patientName} has shared a {notification[el].title} record with you
-                </div>
-            </Link >
-        })
-    }
-    if (notification && !props.doctor) {
-        displayNotifications = Object.keys(notification).map(el => {
-            console.log(notification[el].doctorId, notification[el].masterId)
-            return (
-                <div className="friendlist__requests">
-                    <div style={{ height: "7em", width: "7em" }}>
-                        <img src={notification[el].photo} style={{width:"100%",height:"100%"}} className={`friendlist__patientPhoto  ${darkMode && "friendlist__imgDark"}`} alt="Doctor Dp" />
-                    </div>
-                    <h2>{notification[el].name}</h2>
-                    <h4>Doctor Identifier: {notification[el].doctorId}</h4>
-                    <h4>Document Title: {notification[el].title}</h4>
-                    <div className="shareDocuments__selectTime">
-                        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '2em' }}>
-                            <button style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'lightgreen', borderRadius: '1rem', padding: '0.5rem', cursor: 'pointer' }} onClick={() =>{
-                                tempData.doctorId=notification[el].doctorId;
-                                tempData.status='granted';
-                                tempData.masterId=notification[el].masterId
-                                document.querySelector(".notification__sharePopup").classList.toggle("active")} }>
-                                <span style={{ color: 'black' }}>Accept</span><CheckIcon className="friendlist__userIconAdd" />
-                            </button>
-                            <button style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'red', borderRadius: '1rem', padding: '0.5rem', cursor: 'pointer' }} onClick={() => userDecision(notification[el].doctorId, 'rejected', notification[el].masterId)}>
-                                <span style={{ color: 'black' }}>Reject</span><XIcon className="friendlist__userRemoveIcon" />
-                            </button>
-                        </div>
-                    </div>
-                </div>)
-        })
-    }
-    return (
-        <div className='notification' style={{ backgroundColor: 'white', height: '100vh', width: '100%' }}>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "80%", margin: '0 auto', position: 'relative', paddingTop: '2rem' }}>
-                <p style={{ position: 'absolute', right: '0', top: "1rem", border: 'black 1px solid', padding: '2px' }} onClick={markRead}>Mark all as Read</p>
-                {notification.length === 0 && (
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 auto', marginTop: '3em' }}>
-                        <img style={{ width: "50%" }} src={bell} alt="No request " />
-                        <h2 style={{ color: 'lightcoral' }}>All caught Up!!</h2>
-                    </div>)}
-                {displayNotifications}
-                <form className="notification__sharePopup" onSubmit={(e)=>{e.preventDefault();userDecision(tempData.doctorId, tempData.status, tempData.masterId)}}>
-                    <h2>Share</h2>
-                    <div className="notification__shareTime">
-                        <input ref={timeNumber} type="number" />
-                        <select ref={timeUnit}>
-                            <option value="day">day</option>
-                            <option value="hr">hr</option>
-                            <option value="min">min</option>
-                        </select>
-                    </div>
-                    <div className="notification__popupButtons">
-                        <button type="button" onClick={()=>document.querySelector(".notification__sharePopup.active").classList.remove("active")}>Close</button>
-                        <button type="submit">Share</button>
-                    </div>
-                </form>
+  if (notification && props.doctor) {
+    displayNotifications = Object.keys(notification).map((el) => {
+      return (
+        <Link
+          style={{ width: "100%" }}
+          onClick={() => {
+            dispatch(patientDataDoctorNotification(notification[el].patientId));
+            dispatch(
+              documentGet({
+                filename: notification[el].documentId.concat(".pdf"),
+              })
+            );
+            dispatch(recentPatients(notification[el].patientId));
+          }}
+          key={el}
+          to="/Doctor/documentViewer"
+        >
+          <div
+            className={`${
+              notification[el].markAsRead === "false"
+                ? "notification__display"
+                : "notification__displayRed"
+            }`}
+          >
+            <div style={{ width: "3em", height: "3em" }}>
+              <img
+                src={notification[el].photo}
+                alt="Patient DP"
+                className={`notification__patientPhoto  ${
+                  darkMode && "notification__imgDark"
+                }`}
+              />
             </div>
+            {notification[el].patientName} has shared a {notification[el].title}{" "}
+            record with you
+          </div>
+        </Link>
+      );
+    });
+  }
+  if (notification && !props.doctor) {
+    displayNotifications = Object.keys(notification).map((el) => {
+      console.log(notification[el].doctorId, notification[el].masterId);
+      return (
+        <div className="friendlist__requests">
+          <div style={{ height: "7em", width: "7em" }}>
+            <img
+              src={notification[el].photo}
+              style={{ width: "100%", height: "100%" }}
+              className={`friendlist__patientPhoto  ${
+                darkMode && "friendlist__imgDark"
+              }`}
+              alt="Doctor Dp"
+            />
+          </div>
+          <h2>{notification[el].name}</h2>
+          <h4>Doctor Identifier: {notification[el].doctorId}</h4>
+          <h4>Document Title: {notification[el].title}</h4>
+          <div className="shareDocuments__selectTime">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "2em",
+              }}
+            >
+              <button
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  backgroundColor: "lightgreen",
+                  borderRadius: "1rem",
+                  padding: "0.5rem",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  tempData.doctorId = notification[el].doctorId;
+                  tempData.status = "granted";
+                  tempData.masterId = notification[el].masterId;
+                  document
+                    .querySelector(".notification__sharePopup")
+                    .classList.toggle("active");
+                }}
+              >
+                <span style={{ color: "black" }}>Accept</span>
+                <CheckIcon className="friendlist__userIconAdd" />
+              </button>
+              <button
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  backgroundColor: "red",
+                  borderRadius: "1rem",
+                  padding: "0.5rem",
+                  cursor: "pointer",
+                }}
+                onClick={() =>
+                  userDecision(
+                    notification[el].doctorId,
+                    "rejected",
+                    notification[el].masterId
+                  )
+                }
+              >
+                <span style={{ color: "black" }}>Reject</span>
+                <XIcon className="friendlist__userRemoveIcon" />
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      );
+    });
+  }
+  return (
+    <div
+      className="notification"
+      style={{ backgroundColor: "white", height: "100vh", width: "100%" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "80%",
+          margin: "0 auto",
+          position: "relative",
+          paddingTop: "2rem",
+        }}
+      >
+        <p
+          style={{
+            position: "absolute",
+            right: "0",
+            top: "1rem",
+            border: "black 1px solid",
+            padding: "2px",
+          }}
+          onClick={markRead}
+        >
+          Mark all as Read
+        </p>
+        {notification.length === 0 && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: "0 auto",
+              marginTop: "3em",
+            }}
+          >
+            <img style={{ width: "50%" }} src={bell} alt="No request " />
+            <h2 style={{ color: "lightcoral" }}>All caught Up!!</h2>
+          </div>
+        )}
+        {displayNotifications}
+        <form
+          className="notification__sharePopup"
+          onSubmit={(e) => {
+            e.preventDefault();
+            userDecision(tempData.doctorId, tempData.status, tempData.masterId);
+          }}
+        >
+          <h2>Share</h2>
+          <div className="notification__shareTime">
+            <input ref={timeNumber} type="number" />
+            <select ref={timeUnit}>
+              <option value="day">day</option>
+              <option value="hr">hr</option>
+              <option value="min">min</option>
+            </select>
+          </div>
+          <div className="notification__popupButtons">
+            <button
+              type="button"
+              onClick={() =>
+                document
+                  .querySelector(".notification__sharePopup.active")
+                  .classList.remove("active")
+              }
+            >
+              Close
+            </button>
+            <button type="submit">Share</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Notification;
